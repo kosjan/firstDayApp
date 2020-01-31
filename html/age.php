500b2f3ec55dcb4c2a591aa9848ccce956989e50
kosjan <akishe14@gmail.com>
2020-01-31 14:39:55 +0400

    Добавленна функция вычисления возраста человека до дня

--- html/age.php
+++ html/age.php
@@ -35,15 +35,34 @@
 </form>
 <?php
 if(!empty($_POST['age'])){
-    $age = $_POST['age'];
-      echo "Если вы прожили: ";
-      echo $age;
-      echo " секунд, ";
-    $age_year = $age/365/24/60/60;    
-      echo "то вам: ";
-      echo round($age_year);
-      echo " лет!";
+  function Modulo ($num1)
+  {
+    $buff = intdiv($num1,1);
+    $res = $num - $buff;
   }
+    $age = $_POST['age'];
+      print "Если вы прожили: ";
+      echo $age;
+      print " секунд, ";
+    $age_year = intdiv($age,31536000);
+    $age_grover = Modulo($age_year);
+    $mounth = $age_grover*12;
+    $week = Modulo($mounth)*4;
+    $day = Modulo($week)*7;
+    $hour = Modulo($day)*24;
+      print "то вам: ";
+      echo $age_year;
+      print " лет, ";
+      echo round($mounth);
+      print " месяцев, ";
+      echo round($week);
+      print " недель, ";
+      echo round($day);
+      print " дней, ";
+      echo round($hour);
+      print " часов!";
+  }
+
 ?>
   </main>
   <footer>
